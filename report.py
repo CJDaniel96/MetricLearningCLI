67d8d228e2006a5f01f9058099076750817ecd0e
USIGLOBAL\daniel_tsai <daniel_tsai@usiglobal.com>
2024-03-01 14:05:08 +0800

    update

--- report.py
+++ report.py
@@ -17,7 +17,7 @@ def loss_vision(loss):
     plt.legend()
     plt.savefig(os.path.join(loss, 'loss_record.jpg'))
 
-def csv_vision(csv, csv_target_label):
+def csv_hist_vision(csv, csv_target_label):
     result_df = pd.read_csv(csv)
     ok_score = []
     ng_score = []
@@ -29,12 +29,38 @@ def csv_vision(csv, csv_target_label):
 
     plt.hist([ng_score, ok_score], label = ['NG Similarity Score', 'OK Similarity Score'], bins=50, stacked=True)
     plt.title('Query Image is OK')
+    plt.xlabel('Similarity Score')
+    plt.ylabel('Images Stack')
     plt.legend()
     plt.savefig(os.path.join(os.path.dirname(csv), 'result_hist.jpg'))
 
+def csv_scatter_vision(csv):
+    plt.clf()
+    result_df = pd.read_csv(csv)
+    classes_dict = {}
+    for i in range(len(result_df)):
+        classes_name = os.path.basename(os.path.dirname(result_df.loc[i, 'ImagePath']))
+        if classes_name in classes_dict:
+            classes_dict[classes_name].append(result_df.loc[i, 'Score'])
+        else:
+            classes_dict[classes_name] = [result_df.loc[i, 'Score']]
+
+    count = 1
+    for key in classes_dict:
+        color = 'r' if count == 1 else 'g' if count == 2 else 'b' if count == 3 else 'y'
+        plt.scatter(x=[count] * len(classes_dict[key]), y=classes_dict[key], c=color, label=key)
+        count += 1
+    plt.ylim([-1, 2])
+    plt.title('Data Scatter')
+    plt.xlabel('Labels')
+    plt.ylabel('Similarity Score')
+    plt.legend()
+    plt.savefig(os.path.join(os.path.dirname(csv), 'result_scatter.jpg'))
+
 def main(csv, loss, csv_target_label):
     if csv:
-        csv_vision(csv, csv_target_label)
+        csv_hist_vision(csv, csv_target_label)
+        csv_scatter_vision(csv)
     elif loss:
         loss_vision(loss)
 
